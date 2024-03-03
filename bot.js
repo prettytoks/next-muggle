addc83d81ed32c78f18913d54e0f3c98a7f969ce
Cindy Ogidi <prettytoks@gmail.com>
2024-03-03 18:49:59 +0100

    update start command

--- bot.js
+++ bot.js
@@ -1,16 +1,15 @@
 const { Telegraf } = require('telegraf');
-require('dotenv').config()
+require('dotenv').config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 // Command handler for /start command
-bot.start((ctx) => ctx.reply('Welcome to my Muggle Link bot!'));
+bot.start((ctx) => ctx.reply('Welcome to my Muggle Link bot! \nUse /payment to get the payment link \nUse /share to launch the nextmuggle MiniApp'));
 
 // Command handler for /payment command
 bot.command('payment', async (ctx) => {
     try {
          const paymentLink = "https://muggle-link.onrender.com";
-         
          await ctx.reply("Here's the payment link:\n" + paymentLink);
     } catch (error) {
         console.error('Error handling payment:', error);
@@ -20,10 +19,11 @@ bot.command('payment', async (ctx) => {
 
 // Command handler for /share command
 bot.command('share', (ctx) => {
- 
     const shareContent = 'Check out my awesome MiniApp, share with your friends: https://t.me/nextmuggle_bot/nextmuggle';
-
     ctx.reply(shareContent);
 });
 
-bot.launch();
+// Launch the bot
+bot.launch()
+    .then(() => console.log('Bot started successfully'))
+    .catch((error) => console.error('Error starting bot:', error));
